513409395ca42e577ff6d856fda93c081017bf7d
joshgryzen <58818027+joshgryzen@users.noreply.github.com>
2026-05-09 09:38:24 -0500

    Initial commit

---
+++ Project2.py
@@ -0,0 +1,6 @@
+# Goal implement Nearest Neighbor's heuristic with Simulated Annealing
+# Nearest Neighbor survey's the closes nodes
+# Simulated annealing helps escape local minima by adding in some randomness (sometimes accepting worse solutions) to exploring the search space 
+# - Starts with high temperature (more likely to start in a random direction) then the temperature cools to be more predictable when close to the goal
+
+
